ff44954d97016b5d6a2922fed901d181ddd22a0a
Gnanadeepikavarma <deepika.lakkamraju5@gmail.com>
2023-10-10 09:02:51 +0530

    Code Commited

---
+++ src/constants/index.js
@@ -0,0 +1,80 @@
+export const sortCategoryData = ['All', 'Popular', 'Recommended', 'More'];
+
+export const categoriesData = [
+    {
+        title: 'Ocean',
+        image: require('../../assets/images/ocean.png')
+    },
+    {
+        title: 'Temple',
+        image: require('../../assets/images/temple.png')
+    },
+    {
+        title: 'Mountain',
+        image: require('../../assets/images/mountain.png')
+    },
+    {
+        title: 'Camp',
+        image: require('../../assets/images/camp.png')
+    },
+    {
+        title: 'Sunset',
+        image: require('../../assets/images/sunset.png')
+    },
+    {
+        title: 'Hiking',
+        image: require('../../assets/images/hiking.png')
+    },
+    {
+        title: 'Beach',
+        image: require('../../assets/images/beach.png')
+    },
+    {
+        title: 'Forest',
+        image: require('../../assets/images/forest.png')
+    },
+    
+]
+export const destinationData = [
+    {
+        title: 'Mountain',
+        duration: '12 Days',
+        distance: '400 KM',
+        weather: '20 C',
+        price: 1200,
+        shortDescription: "Mount Kilimanjaro is a dormant volcano located in Kilimanjaro Region of Tanzania.",
+        longDescription: "Mount Kilimanjaro is a dormant volcano located in Kilimanjaro Region of Tanzania. It has three volcanic cones: Kibo, Mawenzi, and Shira. It is the highest mountain in Africa and the highest single free-standing mountain above sea level in the world: 5,895 m above sea level and about 4,900 m above its plateau base.",
+        image: require('../../assets/images/mountain.png')
+    },
+    {
+        title: 'Beach',
+        duration: '7 Days',
+        distance: '450 KM',
+        weather: '30 C',
+        price: 3000,
+        shortDescription: "Varkala is a town in the south Indian state of Kerala",
+        longDescription: "Varkala is a town in the south Indian state of Kerala. It’s on the Arabian Sea and known for Varkala Beach, backed by palm-covered red cliffs. Just south, Papanasam Beach is believed to have holy waters. On a nearby hill, the ancient Janardanaswamy Temple is a Hindu pilgrimage site dedicated to Vishnu.",
+        image: require('../../assets/images/ocean.png')
+    },
+    
+    {
+        title: 'Temple',
+        duration: '5 Days',
+        distance: '299 KM',
+        weather: '30 C',
+        price: 1000,
+        shortDescription: "Shree Padmanabhaswamy Temple is a Hindu temple, dedicated to Vishnu, in Thiruvananthapuram",
+        longDescription: "The Shree Padmanabhaswamy Temple is a Hindu temple, dedicated to Vishnu, in Thiruvananthapuram, the capital of the state of Kerala, India. It is widely considered as the world's richest Hindu temple. The name of the city of 'Thiruvananthapuram' in Malayalam and Tamil translates to The City of Ananta",
+        image: require('../../assets/images/temple.png')
+    },
+    {
+        title: 'wayanad',
+        duration: '20 Days',
+        distance: '604 KM',
+        weather: '34 C',
+        price: 400,
+        shortDescription: "Wayanad is a rural district in Kerala state, southwest India.",
+        longDescription:"Wayanad is a rural district in Kerala state, southwest India. In the east, the Wayanad Wildlife Sanctuary is a lush, forested region with areas of high altitude, home to animals including Asiatic elephants, tigers, leopards and egrets. In the Ambukuthi Hills to the south, Edakkal Caves contain ancient petroglyphs, some dating back to the Neolithic age",
+        image: require('../../assets/images/forest.png')
+    },
+]
